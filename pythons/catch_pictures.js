78c0fef43b598569b622e666be8d22e792e69b97
Laxiflora <liu2000113@gmail.com>
2023-10-07 00:53:56 +0800

    pythons_complete

--- pythons/catch_pictures.js
+++ pythons/catch_pictures.js
@@ -6,7 +6,7 @@ function get_screenshot(){
 }
 
 
-var path = "/mnt/shared/Pictures/convience_store_servant/";
+var path = "/mnt/shared/Pictures/pythons/";
 var img = get_screenshot();
 img = images.clip(img, 125, 743, 190, 300);
-images.save(img, path+"midnight.jpg", "jpg", 100);
+images.save(img, path+"door.jpg", "jpg", 100);
